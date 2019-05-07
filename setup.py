7fb192869f59adbd12dc1c70c552fa4a09a14aa4
Stefan Küpper <stefan.kuepper@posteo.de>
2019-05-07 20:51:08 +0200

    new version 0.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='maxcube-mqtt',
-    version='0.0.3',
+    version='0.0.5',
     description='Control your eQ-3/ELV MAX! Cube with MQTT',
     long_description=readme,
     author='David Uebelacker',
